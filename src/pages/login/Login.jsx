5f9ceccb8058e76d5e690bdb703194ff7074efb9
ElianeRiveraCastillo22 <elianercast@gmail.com>
2022-05-12 01:37:07 -0500

    style: login versión movil

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,18 +1,25 @@
 import { Link } from "react-router-dom";
 import '../login/login.scss';
 const noteStorm= require("../../img/noteStorm.png");
+const google= require("../../img/google.png")
 function Login(){
     return(
-        <div>
-            <img className="logo-NoteStorm" src={noteStorm} alt="" />
-            <input type="text" />
-            <input type="text" />
-            <button>Iniciar sesión</button>
-            <p>¿No tienes una cuenta? <Link to="registro">únete</Link></p>
-            <p>o inicia sesión con:</p>
-            <img src="" alt="" />
+        <div className="containerLogin">
+            <img className="logoNoteStorm" src={noteStorm} alt="" />
+            <div className="formLogin">
+                <input placeholder="Correo electronico" type="text" />
+                <input placeholder="Contraseña" type="text" />
+                <div className="recuperarContraseña">
+                    <p>Recuperar contraseña</p>
+                </div>
+                <button>Iniciar sesión</button>
+                <div className="textLinkRegistro">
+                    <p>¿No tienes una cuenta? <Link to="registro">únete</Link></p>
+                    <p>o inicia sesión con:</p>
+                </div>
+            </div>
+            <img  className="logoGoogle" src={google} alt="" />
         </div>
-
     )
 }
 export default Login
